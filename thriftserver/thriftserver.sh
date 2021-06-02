889db7a480a1d44819e98f9730e41319e2f32d56
gschmutz <guido.schmutz@trivadis.com>
2021-06-02 21:38:29 +0200

    add thriftserver and update Dockerfile according to 3.0.1

---
+++ thriftserver/thriftserver.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export SPARK_MASTER_HOST=`hostname`
+
+. "/spark/sbin/spark-config.sh"
+
+. "/spark/bin/load-spark-env.sh"
+
+mkdir -p $SPARK_THRIFTSERVER_LOG
+
+export SPARK_HOME=/spark
+export CLASS="org.apache.spark.sql.hive.thriftserver.HiveThriftServer2"
+
+ln -sf /dev/stdout /spark/logs/spark-root-org.apache.spark.sql.hive.thriftserver.HiveThriftServer2-1-$SPARK_MASTER_HOST.out
+
+cd /spark/bin && /spark/bin/spark-submit --class $CLASS 1 --name "Thrift JDBC/ODBC Server" "$@" >> $SPARK_THRIFTSERVER_LOG/spark-thriftserver.out
