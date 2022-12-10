80c085a96547ec040eefa794ae56fbc88c205a91
Guido Schmutz <guido.schmutz@trivadis.com>
2022-12-10 15:05:01 +0100

    add build and push script

---
+++ push.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+TAG=3.1.3-hadoop3.3
+
+docker push trivadis/apache-spark-base:${TAG}
+docker push trivadis/apache-spark-master:${TAG}
+docker push trivadis/apache-spark-worker:${TAG}
+docker push trivadis/apache-spark-submit:${TAG}
+docker push trivadis/apache-spark-thriftserver:${TAG}
+docker push trivadis/apache-spark-template-python:${TAG}
