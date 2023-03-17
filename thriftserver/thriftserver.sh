de771c96a045c7553905d12e53d5a57152b0f611
Guido Schmutz <guido.schmutz@trivadis.com>
2023-03-17 09:25:43 +0100

    add --master to specify spark master node

--- thriftserver/thriftserver.sh
+++ thriftserver/thriftserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export SPARK_MASTER_HOST=`hostname`
+# export SPARK_MASTER_HOST=`hostname`
 
 . "/spark/sbin/spark-config.sh"
 
@@ -13,4 +13,4 @@ export CLASS="org.apache.spark.sql.hive.thriftserver.HiveThriftServer2"
 
 ln -sf /dev/stdout /spark/logs/spark-root-org.apache.spark.sql.hive.thriftserver.HiveThriftServer2-1-$SPARK_MASTER_HOST.out
 
-cd /spark/bin && /spark/bin/spark-submit --class $CLASS 1 --name "Thrift JDBC/ODBC Server" "$@" >> $SPARK_THRIFTSERVER_LOG/spark-thriftserver.out
+cd /spark/bin && /spark/bin/spark-submit --class $CLASS 1 --master $SPARK_MASTER --name "Thrift JDBC/ODBC Server" "$@" >> $SPARK_THRIFTSERVER_LOG/spark-thriftserver.out
